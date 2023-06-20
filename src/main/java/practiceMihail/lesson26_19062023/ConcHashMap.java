63eb4fda3848398fdee121827ecad254386fb06e
SergeyZ <szabolotnyi777@gmail.com>
2023-06-20 09:33:42 +0200

    Lessons26 19.06.2023

--- src/main/java/practiceMihail/lesson26_19062023/ConcHashMap.java
+++ src/main/java/practiceMihail/lesson26_19062023/ConcHashMap.java
@@ -6,30 +6,45 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcHashMap {
-    public static void main(String[] args) {
-        
-        Map<Integer,String> map = new ConcurrentHashMap<>();
-        map.put(1,"Q");
-        map.put(2,"W");
-        map.put(3,"E");
-        map.put(4,"R");
-        map.put(5,"T");
+    public static void main(String[] args) throws InterruptedException {
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1, "Q");
+        map.put(2, "W");//!!!
+        map.put(3, "A");
+        map.put(4, "E");
+        map.put(5, "F");
 
-        Runnable R1 = ()-> {
+        Runnable R1 = () -> {
             Iterator<Integer> iterator = map.keySet().iterator();
-            while (iterator.hasNext()){
+            while (iterator.hasNext()) {
                 try {
                     Thread.sleep(800);
-                } catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 Integer i = iterator.next();
-                System.out.println(i + " : "+ map.get(i));
+                System.out.println(i + " : " + map.get(i));
             }
         };
-        Runnable R2 = ()-> {
 
+        Runnable R2 = () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(99, "UU");
         };
 
+        Thread T1 = new Thread(R1);
+        Thread T2 = new Thread(R2);
+
+        T1.start();
+        T2.start();
+
+        T1.join();
+        T2.join();
+
+        System.out.println(map);
     }
 }
